7b20fee7d8b1165c40f00866c623c4a04c842820
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-02-08 20:35:27 +0100

    feat: add load fun to create aliases

--- zsh-pludo.plugin.zsh
+++ zsh-pludo.plugin.zsh
@@ -1,4 +1,5 @@
 BASE_DIR=$(dirname $0)
+CONFIG="$BASE_DIR/config.json"
 
 alias c.="code ."
 alias c="code -g"
@@ -10,31 +11,38 @@ alias activate=". .venv/bin/activate"
 
 alias sido=sudo
 
-alias carbo="cargo build"
-alias carro="cargo run"
-alias carco="cargo check"
-
-
 __get_project_type() {
   local res=""
   local found=0
 
-  for type in $(jq 'keys[]' -r < "$BASE_DIR/config.json"); do 
+  for type in $(jq 'keys[]' -r <"$CONFIG"); do
     if [[ -f "./$type" ]]; then
       res=$type
-      found=$(($found+1))
+      found=$(($found + 1))
     fi
   done
-  
-  if [[ $found = 0 ]]; then 
+
+  if [[ $found = 0 ]]; then
     echo No project found
+    return 1
   elif [[ $found = 1 ]]; then
     echo $res
+    return 0
   else
     echo Multiple projects found
+    return 2
   fi
 }
 
 load() {
+  local type=$(__get_project_type)
+  if [[ "$?" != 0 ]]; then
+    return 1
+  fi
 
+  local project_config=$(jq '."'$type'"' <"$CONFIG")
+
+  for cmd in $(jq 'keys[]' -r <<<"$project_config"); do
+    alias "$cmd=$(jq --raw-output '."'$cmd'"' <<<"$project_config")"
+  done
 }
