510482f39b3b2cce0e2fb0a15648990c88c4c7f3
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-04-26 16:43:45 +0200

    feat: store files as metadata in config entry

--- zsh-pludo.plugin.zsh
+++ zsh-pludo.plugin.zsh
@@ -2,6 +2,9 @@ BASE_DIR=$(dirname $0)
 CONFIG="$BASE_DIR/config.json"
 ORIG_CD=__orig_cd
 
+CONFIG_FILES=__files__
+CONFIG_NAME=__name__
+
 alias c.="code ."
 alias c="code -g"
 
@@ -17,15 +20,31 @@ __get_project_type() {
   local found=0
 
   for type in $(jq 'keys[]' -r <"$CONFIG"); do
-    if [[ -f "./$type" ]]; then
-      res=$type
-      found=$(($found + 1))
-    fi
+    local project_config=$(__get_config $type)
+
+    for file in $(jq ".${CONFIG_FILES}[]" -r <<< "$project_config"); do
+      if [[ -f "./$file" ]]; then
+        res=$type
+        found=$(($found + 1))
+      fi
+    done
   done
 
   if [[ $found = 1 ]]; then
     echo $res
   fi
+}
+
+__get_config() {
+  jq '."'$1'"' <"$CONFIG"
+}
+
+__get_cmds() {
+  for cmd in $(jq 'keys[]' -r <<<"$1"); do
+    if [[ ! $cmd == __* ]]; then
+      echo $cmd "$(jq --raw-output '."'$cmd'"' <<<"$1")"
+    fi
+  done
 }
 
 __set_orig_cd() {
@@ -48,13 +67,11 @@ __pludo_load() {
     return 1
   fi
 
-  local project_config=$(jq '."'$type'"' <"$CONFIG")
+  local project_config=$(__get_config $type)
 
   export PLUDO_PROJECT_TYPE="$type"
 
-  for cmd in $(jq 'keys[]' -r <<<"$project_config"); do
-    alias "$cmd=$(jq --raw-output '."'$cmd'"' <<<"$project_config")"
-  done
+  __get_cmds $project_config | while read -r name cmd; do alias "$name=$cmd"; done
 }
 
 __pludo_unload() {
@@ -66,24 +83,25 @@ __pludo_unload() {
 
   unset PLUDO_PROJECT_TYPE
 
-  local project_config=$(jq '."'$type'"' <"$CONFIG")
+  local project_config=$(__get_config $type)
 
-  for cmd in $(jq 'keys[]' -r <<<"$project_config"); do
-    unalias "$cmd" &> /dev/null
-  done
+  __get_cmds $project_config | while read -r name _; do unalias "$name" &> /dev/null; done
 }
 
 __pludo_status() {
   if [ "$PLUDO_PROJECT_TYPE" != "" ]; then
 
-    local project_config=$(jq '."'$PLUDO_PROJECT_TYPE'"' <"$CONFIG")
-    local project_name="$(jq -r '.__name' <<<"$project_config")"
+    local project_config=$(__get_config $PLUDO_PROJECT_TYPE)
+    local project_name="$(jq -r ".$CONFIG_NAME" <<<"$project_config")"
     if [[ "$project_name" == "null" ]]; then
       export project_name="custom"
     fi
 
     echo "Pludo status: active"
     echo "- Project type: $project_name"
+    echo "- Loaded aliases:"
+
+    __get_cmds $project_config | while read -r name cmd; do echo "  * $name -> $cmd"; done
   else
     echo "Pludo status: inactive"
   fi
