50628ef376ceb82219afa3f1133bb50f2f7d113d
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-04-26 11:04:23 +0200

    feat: add pludo command

--- zsh-pludo.plugin.zsh
+++ zsh-pludo.plugin.zsh
@@ -41,7 +41,7 @@ __set_orig_cd() {
   fi
 }
 
-load() {
+__pludo_load() {
   local type=$(__get_project_type)
 
   if [[ $type == "" ]]; then
@@ -50,17 +50,21 @@ load() {
 
   local project_config=$(jq '."'$type'"' <"$CONFIG")
 
+  export PLUDO_PROJECT_TYPE="$type"
+
   for cmd in $(jq 'keys[]' -r <<<"$project_config"); do
     alias "$cmd=$(jq --raw-output '."'$cmd'"' <<<"$project_config")"
   done
 }
 
-unload() {
+__pludo_unload() {
   local type=$(__get_project_type)
 
   if [[ $type == "" ]]; then
     return 1
   fi
+
+  unset PLUDO_PROJECT_TYPE
 
   local project_config=$(jq '."'$type'"' <"$CONFIG")
 
@@ -69,10 +73,44 @@ unload() {
   done
 }
 
+__pludo_status() {
+  if [ "$PLUDO_PROJECT_TYPE" != "" ]; then
+
+    local project_config=$(jq '."'$PLUDO_PROJECT_TYPE'"' <"$CONFIG")
+    local project_name="$(jq -r '.__name' <<<"$project_config")"
+    if [[ "$project_name" == "null" ]]; then
+      export project_name="custom"
+    fi
+
+    echo "Pludo status: active"
+    echo "- Project type: $project_name"
+  else
+    echo "Pludo status: inactive"
+  fi
+}
+
+pludo() {
+    if [ "$1" = "status" ]; then
+      __pludo_status
+    elif [ "$1" = "load" ]; then
+      __pludo_load
+    elif [ "$1" = "unload" ]; then
+      __pludo_unload
+    else
+      echo "Invalid command usage"
+      echo "Syntax: pludo <cmd>"
+      echo "  cmd :"
+      echo "  - status"
+      echo "  - load"
+      echo "  - unload"
+    fi
+}
+
 __set_orig_cd
+__pludo_load
 
 cd () {
-  unload
+  __pludo_unload
   __orig_cd "${@}"
-  load
+  __pludo_load
 }
